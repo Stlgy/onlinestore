3d8ce857c84c616c5ab08f5eb5dc094aa4db9ce6
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-13 10:28:21 +0100

    Create head.php

---
+++ app/libraries/head.php
@@ -0,0 +1,10 @@
+<?php 
+//echo realpath('.');
+?>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible">
+<meta name= "viewport" content="width=device-width, initial-scale=1.0">
+<title>SHOP</title>
+<link rel="stylesheet" href="template/css/style.css" type="text/css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
