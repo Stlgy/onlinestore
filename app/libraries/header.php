a79bac71070aa59e85357f38cd5eb7a846d2ba72
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-13 10:28:36 +0100

    Create header.php

---
+++ app/libraries/header.php
@@ -0,0 +1,45 @@
+<?php
+
+?>
+<header>
+    <div class="container-fluid">
+        <div class="row  justify-content-between">
+            <div class="col-md-4 links-menu">
+            <a href="index.php"><i class="bi bi-house-fill"></i></a>
+            <a href="about.php"><i class="bi bi-info-circle-fill"></i></a>
+            <a href="views/product/view.php"><i class="bi bi-shop-window"></i></a>
+            </div>
+            <div class="col-md-8 ">
+                <nav class="nav-login">
+                
+                    <ul class="menu-main">
+                        <?php 
+                            if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
+                                ?>
+                                <h1 id="Welcome">welcome <?= $_SESSION['username']; ?></h1>
+                                <?php                       
+                            }
+                        ?>
+                        
+                        <?php
+                            //$_SESSION['username'] = 0;
+                            if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
+                                ?>
+                                    <li><a class="logs" href="login.php">LOGIN</a></li>
+                                    <li><a class="logs" href="signup.php">SIGN UP</a></li>
+                                <?php
+                            }else{
+                                ?>  
+                                    <li><a class="logs" href="controllers/cartController.php?q=cart">Cart</a></li>
+                                    <li><a class="logs" href="controllers/usersController.php?q=logout">Logout</a></li>
+                                    <li><a class="logs" href="update-profile.php">Setthings</a></li>
+                                <?php
+                            }
+                        ?>
+                    </ul>
+                
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
