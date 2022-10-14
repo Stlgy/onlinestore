436caffc7e57d0133571b7d5841c696369b761ee
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-14 16:30:55 +0100

    Update product.php

--- app/models/product.php
+++ app/models/product.php
@@ -1,1 +1,39 @@
+<?php
+    //require_once '../libraries/start.php';
+    if ($_SESSION["getData"] == 0) {
+        require_once '../libraries/start.php';
+        include('../libraries/class_utils.php');
+    }else{
+        require_once 'libraries/start.php';
+    }
 
+
+    class Product extends sys_utils
+    {
+        public function uploadImage($file){
+
+            $res =SQL::run("INSERT INTO " . BDPX . "_products_onlinestore WHERE img_p='$file'" );
+            if($res){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        public function findProduct(){
+            
+        }
+        public function findProducts(){
+
+            $result = [];
+            $res = SQL::run("SELECT *FROM " . BDPX . "_products_onlinestore ORDER BY name_p ASC");
+
+            if($res && $res->num_rows > 0){
+                $result = $res->fetch_assoc();
+            }
+            return $result;
+        }
+        public function addProduct(){
+
+        }
+    }
+?>
