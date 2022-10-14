9da32fa281d16048f16692408ec5b99d3e50b51c
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-14 09:17:17 +0100

    Create nav-admin.php

---
+++ app/libraries/nav-admin.php
@@ -0,0 +1,10 @@
+<?php
+?>
+  <nav class="navAdmin">
+        <p class="productMenu">Manage Products</p>
+        <a href="add-product.php"><i class="bi bi-plus-square-fill"></i> Insert </a>
+        <a href="update-product.php"><i class="bi bi-arrow-clockwise"></i> Update</a>
+        <a href="delete-product.php"><i class="bi bi-trash-fill"></i> Delete</a>
+        <a href="uploads.php"><i class="bi bi-upload"></i> Upload</a>
+    </nav>
+  
